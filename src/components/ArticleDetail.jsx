9e1220e85595f4981799e78ac47f02ebbe2099ca
Vaios-S <vaios.sav.dev@gmail.com>
2026-05-27 23:17:29 +0300

    Build the Article Detail page

--- src/components/ArticleDetail.jsx
+++ src/components/ArticleDetail.jsx
@@ -1,1 +1,24 @@
-export default function ArticleDetail() {}
+import { useParams } from "react-router";
+import { articles } from "../data/articles.js";
+import { Link } from "react-router-dom";
+import NotFound from "./NotFound.jsx";
+
+export default function ArticleDetail() {
+  const { articleId } = useParams();
+
+  const article = articles.find((a) => a.id === parseInt(articleId));
+
+  if (!article) {
+    return <NotFound />;
+  }
+
+  return (
+    <div>
+      <h2>{article.title}</h2>
+      <Link to={`/author/${article.authorId}`}>
+        <h2>{article.authorName}</h2>
+      </Link>
+      <p>{article.body}</p>
+    </div>
+  );
+}
