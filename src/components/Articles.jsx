8b3282eac539757fa1ab91243313eca76d6a8829
Vaios-S <vaios.sav.dev@gmail.com>
2026-05-27 22:53:36 +0300

    Build the Articles list page

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,1 +1,18 @@
-export default function Articles() {}
+import { articles } from "../data/articles.js";
+import { Link } from "react-router-dom";
+
+export default function Articles() {
+  return (
+    <div>
+      {articles.map((article) => (
+        <div key={article.id}>
+          {" "}
+          <Link to={`/articles/${article.id}`}>
+            <h2>{article.title}</h2>
+          </Link>
+          <p>{article.authorName}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
