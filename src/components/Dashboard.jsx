053eca459b098bb3fa50480c55b56d31277358e9
Vaios-S <vaios.sav.dev@gmail.com>
2026-05-29 23:27:40 +0300

    Read state in the Dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,1 +1,28 @@
-export default function Dashboard() {}
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+export default function Dashboard() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!location.state) {
+      navigate("/login");
+    }
+  }, [location.state, navigate]);
+
+  if (!location.state) {
+    return null;
+  }
+
+  const { username, loginTime } = location.state;
+
+  const formattedTime = new Date(loginTime).toLocaleString("el-GR");
+
+  return (
+    <div>
+      <h1>Welcome, {username}</h1>
+      <p>You logged in at {formattedTime}</p>
+    </div>
+  );
+}
