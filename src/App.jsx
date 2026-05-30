fcd3274cfdbf94661429ed66a110026b77d2c39c
Vaios-S <vaios.sav.dev@gmail.com>
2026-05-30 21:41:56 +0300

    Apply lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,45 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { suspense } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import Articles from "./components/Articles";
-import ArticleDetail from "./components/ArticleDetail";
-import Author from "./components/Author";
 import Login from "./components/Login";
-import Dashboard from "./components/Dashboard";
-import NotFound from "./components/NotFound";
-import DashboardSettings from "./components/DashboardSettings";
-import DashboardNewArticles from "./components/DashboardNewArticles";
-import DashboardHome from "./components/DashboardHome";
+import LoadingSpinner from "./components/LoadingSpinner";
+
+const Articles = React.lazy(() => import("./components/Articles"));
+const ArticleDetail = React.lazy(() => import("./components/ArticleDetail"));
+const Author = React.lazy(() => import("./components/Author"));
+const Dashboard = React.lazy(() => import("./components/Dashboard"));
+const NotFound = React.lazy(() => import("./components/NotFound"));
+const DashboardSettings = React.lazy(
+  () => import("./components/DashboardSettings"),
+);
+const DashboardNewArticles = React.lazy(
+  () => import("./components/DashboardNewArticles"),
+);
+const DashboardHome = React.lazy(() => import("./components/DashboardHome"));
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
+      <Suspense fallback={<LoadingSpinner />}>
+        <BrowserRouter>
+          <Navbar />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/articles" element={<Articles />} />
-          <Route path="/articles/:articleId" element={<ArticleDetail />} />
-          <Route path="/author/:authorId" element={<Author />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />}>
-            <Route index element={<DashboardHome />} />
-            <Route path="settings" element={<DashboardSettings />} />
-            <Route path="new-articles" element={<DashboardNewArticles />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/articles" element={<Articles />} />
+            <Route path="/articles/:articleId" element={<ArticleDetail />} />
+            <Route path="/author/:authorId" element={<Author />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />}>
+              <Route index element={<DashboardHome />} />
+              <Route path="settings" element={<DashboardSettings />} />
+              <Route path="new-articles" element={<DashboardNewArticles />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </>
   );
 }
